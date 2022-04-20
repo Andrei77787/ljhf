06901253b9f1f3e91c360c022308f5d8f9d8b8e5
Andrei77787 <kostroma.kpc@gmail.com>
2022-04-20 09:31:09 +0300

    f

---
+++ Library/Application.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Application
+    {
+        public static Application application { get; set; }
+        public List<TaskList> taskLists { get; set; }
+
+        private Application()
+        {
+            taskLists = new List<TaskList>();
+        }
+
+        /// </summary>
+        /// Возвращает ссылку на экземпляр приложения, а в его отсутствие создает экземпляр
+        /// </summary>
+        public Application GetApplication()
+        {
+            if (application == null)
+            {
+                return new Application();
+            }
+
+            return application;
+        }
+
+        /// </summary>
+        /// Создает список задач с указанным именем и добавляет его в taskLists
+        /// </summary>
+        public void CreateTaskList(string name)
+        {
+            taskLists.Add(new TaskList(name));
+        }
+
+        /// </summary>
+        /// Возвращает имена списков задач
+        /// </summary>
+        public List<string> GetTaskListNames()
+        {
+            var names = taskLists.Select(t => t.GetName()).ToList();
+            return names;
+        }
+
+        /// </summary>
+        ///  Возвращает список задач по имени списка
+        ///  </summary>
+        public TaskList GetTaskListNames(string name)
+        {
+            var taskList = taskLists.FirstOrDefault(t => t.GetName() == name);
+            return taskList;
+        }
+
+        /// </summary>
+        /// Получает задачи запланированные на сегодня в хронологическом порядке
+        /// </summary>
+        public List<Task> GetTasksByToday()
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (var taskList in taskLists)
+            {
+                tasks.AddRange(taskList.GetTasksByToday());
+            }
+
+            return tasks;
+        }
+
+        /// </summary>
+        /// Получает задачи запланированные завтра и позже в хронологическом порядке
+        /// </summary>
+        public List<Task> GetTasksByFuture()
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (var taskList in taskLists)
+            {
+                tasks.AddRange(taskList.GetTasksByFuture());
+            }
+
+            return tasks;
+        }
+    }
+}
---
+++ Library/TaskList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class TaskList
+    {
+        private string name { get; set; }
+        private List<Task> tasks { get; set; }
+
+        public TaskList(string name)
+        {
+            this.name = name;
+        }
+
+        ///<summary>
+        /// Возвращает имя списка задач
+        /// </summary>
+        public string GetName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// Добавляет задачу в список задач
+        /// </summary>
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+
+        /// <summary>
+        /// Возвращает список всех задач
+        /// </summary>
+        public List<Task> GetTasks()
+        {
+            return tasks;
+        }
+
+        /// <summary>
+        /// удаляет задачу из списка задач
+        /// </summary>
+        public void Remove(Task task)
+        {
+            if (tasks.Contains(task))
+            {
+                tasks.Remove(task);
+            }
+        }
+
+        /// <summary>
+        /// Возвращает задачи запланированные на сегодня в хронологическом порядке
+        /// </summary>
+        public List<Task> GetTasksByToday()
+        {
+            var today = DateTime.Now.ToShortDateString();
+            var tasksByToday = tasks.Where(t => t.Due.ToShortDateString() == today).ToList();
+            return tasksByToday;
+        }
+
+        /// <summary>
+        /// Возвращает задачи запланированные на завтра и позже в хронологическом порядке
+        /// </summary>
+        public List<Task> GetTasksByFuture()
+        {
+            var today = DateTime.Now;
+            var tasksByToday = tasks.Where(t => t.Due > today).ToList();
+            return tasksByToday;
+        }
+    }
+}
